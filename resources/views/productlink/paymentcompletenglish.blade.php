ff7ba6c2635d6833012fbf2d717d19df4bf7fd54
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-12-12 12:00:56 -0500

    thanks page

--- resources/views/productlink/paymentcompletenglish.blade.php
+++ resources/views/productlink/paymentcompletenglish.blade.php
@@ -1903,6 +1903,134 @@
         </div>
         <div align="center" id="ixcmd" data-secondsdelay="" class="fk-col col-12 col-lg-5 d-none d-lg-flex order-1 order-lg-2 sidebar"></div>
       </div>
+      <form class="edit_checkout" id="edit_checkout_760789837" action="/store" autocomplete="off" accept-charset="UTF-8" method="POST"> @csrf <div class='container-fluid d-lg-none cart-summary-wrapper splitter'>
+      </div>
+      <div class='container'>
+         <div class='row'>
+            <div class='col-12'>
+               <div class='header clearfix'>
+                  <div class='badges-wrapper'>
+                     <img class='secure-logos' src='{{asset('cdn.shopify.com/s/files/1/0522/7433/1830/files/class-payments730e.png?v=1675448311')}}'>
+                  </div>
+               </div>
+               <div class='row space-4'>
+                 <div class='col-12 col-lg-7'>
+                     <div>
+                        <div class='timer-title d-flex align-items-center'>
+                           <div class="timer-fire">
+                              <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="125px" height="189.864px" viewBox="0 0 125 189.864" enable-background="new 0 0 125 189.864" xml:space="preserve">
+                                 <path class="flame-main" fill="#F36E21" d="M76.553,186.09c0,0-10.178-2.976-15.325-8.226s-9.278-16.82-9.278-16.82s-0.241-6.647-4.136-18.465
+                                    c0,0,3.357,4.969,5.103,9.938c0,0-5.305-21.086,1.712-30.418c7.017-9.333,0.571-35.654-2.25-37.534c0,0,13.07,5.64,19.875,47.54
+                                    c6.806,41.899,16.831,45.301,6.088,53.985" />
+                                 <path class="flame-main one" fill="#F6891F" d="M61.693,122.257c4.117-15.4,12.097-14.487-11.589-60.872c0,0,32.016,10.223,52.601,63.123
+                                    c20.585,52.899-19.848,61.045-19.643,61.582c0.206,0.537-19.401-0.269-14.835-18.532S57.576,137.656,61.693,122.257z" />
+                                 <path class="flame-main two" fill="#FFD04A" d="M81.657,79.192c0,0,11.549,24.845,3.626,40.02c-7.924,15.175-21.126,41.899-0.425,64.998
+                                    C84.858,184.21,125.705,150.905,81.657,79.192z" />
+                                 <path class="flame-main three" fill="#FDBA16" d="M99.92,101.754c0,0-23.208,47.027-12.043,80.072c0,0,32.741-16.073,20.108-45.79
+                                    C95.354,106.319,99.92,114.108,99.92,101.754z" />
+                                 <path class="flame-main four" fill="#F36E21" d="M103.143,105.917c0,0,8.927,30.753-1.043,46.868c-9.969,16.115-14.799,29.041-14.799,29.041
+                                    S134.387,164.603,103.143,105.917z" />
+                                 <path class="flame-main five" fill="#FDBA16" d="M62.049,104.171c0,0-15.645,67.588,10.529,77.655C98.753,191.894,69.033,130.761,62.049,104.171z" />
+                                 <path class="flame" fill="#F36E21" d="M101.011,112.926c0,0,8.973,10.519,4.556,16.543C99.37,129.735,106.752,117.406,101.011,112.926z" />
+                                 <path class="flame one" fill="#F36E21" d="M55.592,126.854c0,0-3.819,13.29,2.699,16.945C64.038,141.48,55.907,132.263,55.592,126.854z" />
+                                 <path class="flame two" fill="#F36E21" d="M54.918,104.595c0,0-3.959,6.109-1.24,8.949C56.93,113.256,52.228,107.329,54.918,104.595z" />
+                              </svg>
+                           </div>
+                        </div>
+                     </div>
+                     <div class='section-title d-flex align-items-center mt-2 mt-lg-0'>
+                        <h2> Thank you for your purchase ☺️ <span>{{$shipping_first_name}}</span></h2>
+                        <div class='completed-indicator' data-function='shipping-details-completed-indicator'></div>
+                     </div>
+                     <div class='card' data-function='checkout-form-shipping-section' data-url='KMWCPNRGNJR/shipping_rates.html'>
+
+                        <div class='form-row'>
+                           <div class='form-grouYp col-12'>
+                             <h2 class="form">Your payment is being processed 💳...</h2>
+                              <p>
+                                 Dear {{$shipping_first_name}},
+
+                                 On behalf of the entire team, I would like to express our sincere thanks for choosing our product. It is a real honor for us that you have trusted our brand to meet your needs.
+                                 
+                                 I would like to inform you that your payment has been successfully received and we are working diligently to process your order as soon as possible. We assure you that your purchase will be handled with the utmost care and attention, and we will keep you informed of any updates regarding your order.
+                                 
+                                 We hope you enjoy your VW4 product as much as we enjoyed creating it. If you have any questions or need additional assistance, please feel free to contact us at any time.
+                                 
+                                 Again, thank you for choosing our products and services. We value your business and look forward to the opportunity to serve you in the future.
+                                 
+                                 Sincerely,
+                                 The Checkout Secure Team.
+                              </p>
+                           </div>
+                        </div>
+                     </div>
+                 </div>
+
+                 <div class='col-12 col-lg-5'>
+                     <div class='card'>
+                         <div class='js-cart d-lg-block'>
+                            <div class='cart'>
+                               <div class='item line-row'>
+                                  <div class='d-flex align-items-center'>
+                                     <div class='item-img-wrapper'>
+                                        <div class='item-img'>
+                                           <img src='{{asset($product_image_path)}}'>
+                                        </div>
+                                        <div class='item-qty'>
+                                           {{$product_quantity}}
+                                        </div>
+                                     </div>
+                                     <div class='d-flex flex-column'>
+                                        <span class='bold'>
+                                           {{$product_name}}
+                                        </span>
+                                     </div>
+                                  </div>
+                                  <div class='ml-1 bold price' id="total_1">
+                                    $ {{number_format($product_price, 2, '.', '')}}
+                                  </div>
+                               </div>
+                               <div class='line-row d-block' data-function='cart-total-container'>
+                                  <div class='d-flex justify-content-between'>
+                                     <span> Subtotal </span>
+                                     <span class='[]' id="subtotal_1"> ${{number_format($product_price, 2, '.', '')}}
+                                     </span>
+                                  </div>
+                                  <div class='d-flex justify-content-between space-top-2'>
+                                     <span id="shipping_description_id">
+                                       {{$checkout_free_option_label}}
+                                     </span>
+                                     <span id="shipping_value_id">
+                                       FREE
+                                     </span>
+                                  </div>
+                                  <div class='d-flex justify-content-between space-top-2'>
+                                     <span> Taxes </span>
+                                     <span id="taxes_id">
+                                       {{number_format($checkout_taxes_value, 2, '.', '')}}                                        </span>
+                                  </div>
+                                  <div class='d-flex justify-content-between space-top-2'>
+                                     <span class='bold'> Total </span>
+                                     <span class='bold' data-function='three-d-secure-data-container' data-three-d-secure='eyJlbmFibGVkIjpmYWxzZSwiYW1vdW50IjoiNjkuOTkiLCJjdXJyZW5jeSI6
+                                        IlVTRCIsImVtYWlsIjoiIn0=
+                                        ' data-total-amount='69.99' id="total_2" >
+                                        $ {{number_format($product_price, 2, '.', '')}}
+                                     </span>
+                                  </div>
+                               </div>
+
+                            </div>
+                         </div>
+                         <br>
+                     </div>
+                 </div>
+
+               </div>
+            </div>
+         </div>
+      </div>
+   </form>
+   @include('scripts.thankspage');
     </div>
     <script id="scriptData">
       window.addEventListener('load', (function() {
