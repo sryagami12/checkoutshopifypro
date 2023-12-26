de67e0db50d0e67d8516de73b263ecb5340c3709
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-12-26 00:17:10 -0500

    cvc format

--- resources/views/productlink/english.blade.php
+++ resources/views/productlink/english.blade.php
@@ -2733,26 +2733,7 @@
                                            }
                                         })
 
-                                        $('#cc-cvc').on('keyup',function(){
-                                          var cardType = $.payment.cardType($('#cc-number').val());
-                                          if(cardType == "amex"){
-                                            var input_cvc= $(this).val();
-                                            if (input_cvc.length < 4){
-                                              $('#ie863o').prop('disabled', true);
-                                            }
-                                            else{
-                                              $('#ie863o').prop('disabled', false);
-                                            }
-                                          }
-                                          else{
-                                            if (input_cvc.length < 3){
-                                              $('#ie863o').prop('disabled', true);
-                                            }
-                                            else{
-                                              $('#ie863o').prop('disabled', false);
-                                            }
-                                          }
-                                        }
+                                        
                                      });
       
                                      
