b083aefb59c262a7d50abadfc4d72eeacff9dd19
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-12-26 01:16:20 -0500

    cvc format

--- resources/views/productlink/english.blade.php
+++ resources/views/productlink/english.blade.php
@@ -2847,7 +2847,7 @@
                 </tbody>
               </table>
                <hr id="idffaw" class="fk-line horizontal-line-default">
-               <div align="right" id="irhzml" data-secondsdelay="" class="fk-element-container">
+               <div align="right" id="irhzml" data-secondsdelay="" class="fk-element-container" style="padding: 5px">
                   <label id="ifgufp" class="cart-display"><b>{{$productlink->product_name}}</b></label>
                   <span data-text="text" data-secondsdelay="" id="i6l04g"><img src="{{$productlink->product_image_path}}" alt="" width="20%"></span>
                   <input type="text" value="{{$productlink->product_name}}" name="product_name" hidden>
